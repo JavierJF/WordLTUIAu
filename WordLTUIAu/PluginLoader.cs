d3dd1166b1cf490c82c7f41ca9a9b90b7b32f76e
Javier Jaramago Fernández <jaramago.fernandez.javier@gmail.com>
2017-02-19 00:28:18 +0100

    Initial project structure

---
+++ WordLTUIAu/PluginLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Optional;
+
+namespace WordLTUIAu
+{
+    class PluginLoader
+    {
+        public static Option<ICollection<UIAutomationPlugin>> loadPlugins(String path)
+        {
+            string[] dllFileNames = null;
+
+            if (Directory.Exists(path))
+            {
+                dllFileNames = Directory.GetFiles(path, "*.dll");
+            }
+
+            return Option.None<ICollection<UIAutomationPlugin>>();
+        }
+    }
+}
---
+++ WordLTUIAu/UIAutomationPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordLTUIAu
+{
+    enum ErrorType
+    {
+        TargetNotAvailable,
+        PropertiesNotFound,
+        OptionsNotTriggered
+    }
+
+    class UIAutomationPluginError
+    {
+        private ErrorType err { get; }
+        
+        private String msg { get; }
+
+        public UIAutomationPluginError(ErrorType err, String msg)
+        {
+            this.err = err;
+            this.msg = msg;
+        }
+    }
+
+    interface UIAutomationPlugin
+    {
+        UIAutomationPluginError watch();
+    }
+}
